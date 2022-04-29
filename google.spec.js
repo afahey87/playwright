754f2a9eb320fb31dba4ddfbe4180e09f67c9c26
Adam Fahey <adamfahey@Adams-MacBook-Pro.local>
2022-04-28 20:22:43 -0400

    Searched google for cats movie and verify text

--- google.spec.js
+++ google.spec.js
@@ -6,4 +6,21 @@ test('google test', async ({ page }) => {
 
     // Expect a title "to contain" a substring.
     await expect(page).toHaveTitle(/Google/);
+
+    // Click in search box
+    //await page.click('.gLFyf gsfi');
+
+    // Fill in google search box with cats
+    await page.fill('.gLFyf', 'cats');
+
+    // Click search button
+    await page.click('.gNO89b');
+
+    // Assert cats 2019 film
+    const locator = page.locator('.tF2Cxc:nth-child(7)');
+    await expect(locator).toContainText('Cats (2019) - IMDbhttps://www.imdb.com');
+
+    // Close page
+    await page.close()
+
   });
